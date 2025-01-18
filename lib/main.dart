5f3083c64c06bb6d0fdb11def8f346deffcfa5c4
Jakub Milcarz <jakub.milcarz@accenture.com>
2025-01-18 10:51:44 +0100

    added home and onboarding screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:accenture_hackaton_2025/presentation/features/onboarding/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      home: OnboardingScreen(),
     );
   }
 }
