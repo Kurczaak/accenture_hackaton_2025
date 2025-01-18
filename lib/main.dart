bbb42288ea41acf0312cc7b29021e1500ff9e1f8
Michał Kurczak <kurczakmichal97@gmail.com>
2025-01-18 10:31:57 +0100

    feat: add basic packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,30 @@
+import 'package:accenture_hackaton_2025/router/router.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+
+  final _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp.router(
+      routerConfig: _appRouter.config(
+        deepLinkBuilder: (deepLink) {
+          if (kIsWeb) {
+            return deepLink;
+          }
+          if (deepLink.uri.fragment == '/' || deepLink.uri.fragment.isEmpty) {
+            return DeepLink.defaultPath;
+          }
+          return DeepLink.path(deepLink.uri.fragment);
+        },
       ),
     );
   }
---
+++ lib/router/router.dart
@@ -0,0 +1,15 @@
+import 'package:accenture_hackaton_2025/router/router.gr.dart';
+import 'package:auto_route/auto_route.dart';
+
+@AutoRouterConfig()
+class AppRouter extends RootStackRouter {
+  AppRouter();
+
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(page: HomeRoute.page, initial: true),
+      ];
+
+  @override
+  late final List<AutoRouteGuard> guards = [];
+}
